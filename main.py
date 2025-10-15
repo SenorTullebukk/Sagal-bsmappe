2319134dbc6923724298dd76c7c7703b47d95843
Jonas Andreasen <58115641+SenorTullebukk@users.noreply.github.com>
2025-10-15 16:14:57 +0200

    Todoliste i main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello, World")
+'''
+Todo:
+-Lave preamble for hver type side
+-Lave en funktion som laver deltagerlisten om til en dataframe
+-Laver Postlisten om til en dataframe
+-Lave mapper til hver af de ting som skal laves pdf'er af
+-Lave en funktion som samler pdf'erne til holdbrev og osv.
+-Så er der også lidt øvrige som skal laves.'''
